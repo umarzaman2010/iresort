6fb967a933e38f7a646785d39e2b427c38d3cf14
umarzaman2010 <umarzaman2010@gmail.com>
2019-09-30 22:52:02 +0300

    changes on home done

--- frontend/views/layouts/header2.php
+++ frontend/views/layouts/header2.php
@@ -15,51 +15,13 @@ use yii\helpers\ArrayHelper;
     <?php endif; ?>
 </section><!-- /.content -->
 <header class="s-header">
-<div class="row">
-    <div class="col col-lg-3">
-        <div class="header-logo">
-            <a class="site-logo" href="<?php echo \yii\helpers\Url::to(Yii::getAlias('@web')) ?>">
-<!--                --><?//= \yii\helpers\Html::a('Home', ['/#home']) ?>
+    <div class="header-logo">
+        <a class="site-logo" href="<?php echo \yii\helpers\Url::to(Yii::getAlias('@web')) ?>">
+            <!--                --><?//= \yii\helpers\Html::a('Home', ['/#home']) ?>
 
-                <img src="<?= Yii::getAlias('@web')?>/glimages/services_images/small-white-logo-1316x1047.png" alt="Homepage">
-            </a>
-        </div>
-
+            <img src="<?= Yii::getAlias('@web')?>/glimages/services_images/small-white-logo-1316x1047.png" alt="Homepage">
+        </a>
     </div>
-    <div class="col col-lg-2">
-
-    </div>
-    <div class="col col-lg-3">
-
-    </div>
-    <div class="col col-lg-2" style="background-color: #ffffff; text-align: center; opacity: .5">
-        <div style="background: cornsilk; width:130px;">
-            <!--    <nav class="header-nav">-->
-<!--            <div class="hidden-xs">-->
-                <?php echo Nav::widget([
-                    'options' => ['class' => 'navbar-nav navbar-right'],
-                    'items' => [
-                        [
-                            'label'=>Yii::t('frontend', 'Language'),
-                            'items'=>array_map(function ($code) {
-                                return [
-                                    'label' => Yii::$app->params['availableLocales'][$code],
-                                    'url' => ['/site/set-locale', 'locale'=>$code],
-                                    'active' => Yii::$app->language === $code
-                                ];
-                            }, array_keys(Yii::$app->params['availableLocales']))
-                        ]
-                    ]]);
-                ?>
-<!--            </div>-->
-            <!--    </nav>-->
-        </div>
-    </div>
-    <div class="col col-lg-2">
-
-    </div>
-
-</div>
 
     <nav class="header-nav">
 
@@ -78,11 +40,23 @@ use yii\helpers\ArrayHelper;
 <!--                        <li><a class="smoothscroll"  href="#services" title="services">Services</a></li>-->
                         <li><?= \yii\helpers\Html::a('services', ['/#services']) ?></li>
                         <li><?= \yii\helpers\Html::a('Terms and Condition', ['/user/sign-in/terms']) ?></li>
+
                         <?php if(Yii::$app->user->isGuest){?> <li><?= \yii\helpers\Html::a('Register', ['/user/sign-in/signup'], ['data' => ['method' => 'post']]) ?></li><?php }?>
                 <?php if(Yii::$app->user->isGuest){?> <li><?= \yii\helpers\Html::a('Login', ['/user/sign-in/login'], ['data' => ['method' => 'post']]) ?></li><?php }?>
                 <?php if(!Yii::$app->user->isGuest){?> <li><?= \yii\helpers\Html::a('Logout', ['/user/sign-in/logout'], ['data' => ['method' => 'post']]) ?>
 
                 </li><?php }?>
+
+<hr style="line-height: 10px; outline-color: #ffffff">
+                        <?php
+array_map(function($code){
+                              ?>
+    <li><?= \yii\helpers\Html::a(Yii::t('frontend',Yii::$app->params['availableLocales'][$code]), ['/site/set-locale', 'locale'=>$code]) ?></li>
+
+
+    <?php
+                            }, array_keys(Yii::$app->params['availableLocales']))
+?>
             </ul>
 
 
@@ -104,12 +78,12 @@ use yii\helpers\ArrayHelper;
     <div class="overlay"></div>
     <div class="shadow-overlay"></div>
 
-    <div class="home-content" style="background-image:url('<?= Yii::getAlias('@web') ?>/glimages/services_images/1-1920x1080.jpg')">
+    <div class="home-content" style="background-image:url('<?= Yii::getAlias('@web') ?>/glimages/services_images/1-1920x1080.jpg');background-size: cover">
 
         <div class="row home-content__main">
 
-            <h2 class="mbr-fonts-style display-1" style="color: #000000">True Outdoor Landscape</h2>
-            <p class="lead mbr-text mbr-fonts-style display-5" style="color: #000000">a huge area of landscape of green areas and basketball/tennis play yards and special lane for horse riding/walking/ byscle</p>
+            <h2 class="mbr-fonts-style display-1" style="color: #762D4A">True Outdoor Landscape</h2>
+            <p class="lead mbr-text mbr-fonts-style display-5" style="color: #762D4A">a huge area of landscape of green areas and basketball/tennis play yards and special lane for horse riding/walking/ byscle</p>
 
         </div>
 
@@ -134,12 +108,7 @@ use yii\helpers\ArrayHelper;
         <li>
             <a href="#0"><i class="fa fa-instagram" aria-hidden="true"></i><span>Instagram</span></a>
         </li>
-        <li>
-            <a href="#0"><i class="fa fa-behance" aria-hidden="true"></i><span>Behance</span></a>
-        </li>
-        <li>
-            <a href="#0"><i class="fa fa-dribbble" aria-hidden="true"></i><span>Dribbble</span></a>
-        </li>
+   
     </ul>
     <!-- end home-social -->
 
